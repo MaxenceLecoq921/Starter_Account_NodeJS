0ed54e7c0a2fede02e2b1e29f0bb3ad0159fa210
Maxence <maxencelecoqpro@gmail.com>
2023-03-21 00:02:18 +0100

    test update

--- api/router.js
+++ api/router.js
@@ -1,10 +1,8 @@
 import express from "express"
-import routertest from "./test/router.js"
 import routerusers from "./users/router.js"
 
 const router = express.Router()
 
-router.use('/test',routertest)
 router.use('/users',routerusers)
 
 export default router
--- api/test/router.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import controller from "./controller.js"
-
-const router = express.Router()
-
-// router.get('/getExample', controller.getExample)
-// router.post('/postExample', controller.postExample)
-
-export default router
